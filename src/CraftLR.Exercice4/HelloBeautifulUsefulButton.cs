163951fb80306a880e6494e3f4a9edb3cd7c0f8d
Maxance Bessonnet <maxbess17@icloud.com>
2026-04-20 11:50:45 +0200

    exo 1, 2 3 4 5 et 6

--- src/CraftLR.Exercice4/HelloBeautifulUsefulButton.cs
+++ src/CraftLR.Exercice4/HelloBeautifulUsefulButton.cs
@@ -1,4 +1,6 @@
+using System;
 using Avalonia.Controls;
+using Avalonia.Interactivity;
 
 namespace CraftLR.Exercice4;
 
@@ -7,5 +9,10 @@ public partial class HelloBeautifulUsefulButton : Window
     public HelloBeautifulUsefulButton()
     {
         InitializeComponent();
+    }
+
+    private void Button_OnClick(object sender, RoutedEventArgs eventData)
+    {
+        Console.WriteLine("Hello, beautiful and useful button!");
     }
 }
