2e74db508902eb9d6ba72f567c2b4313d2105846
Maxance Bessonnet <maxbess17@icloud.com>
2026-04-20 12:33:27 +0200

    exo 7

--- src/CraftLR.Exercice7/HelloButtonViewModel.cs
+++ src/CraftLR.Exercice7/HelloButtonViewModel.cs
@@ -1,9 +1,12 @@
+using System.Windows.Input;
+
 namespace CraftLR.Exercice7;
 
 public class HelloButtonViewModel : HelloButtonViewModelBase
 {
     private string _buttonCaption = "Hello!";
     private int _numberOfClicks;
+    private bool _isButtonEnabled = true;
 
     public string ButtonCaption
     {
@@ -16,9 +19,24 @@ public class HelloButtonViewModel : HelloButtonViewModelBase
         }
     }
 
+    public bool IsButtonEnabled
+    {
+        get => _isButtonEnabled;
+        set
+        {
+            if (_isButtonEnabled == value) return;
+            _isButtonEnabled = value;
+            OnPropertyChanged();
+        }
+    }
+
     public void CountClick()
     {
         _numberOfClicks++;
         ButtonCaption = $"{_numberOfClicks} Clicks";
+        if (_numberOfClicks >= 5)
+        {
+            IsButtonEnabled = false;
+        }
     }
 }
